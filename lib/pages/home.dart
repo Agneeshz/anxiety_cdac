6369065120e1d8d85058655a24ef5b519c59c788
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-06-17 17:47:03 +0530

    ADD : Image picker

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,130 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  const Home({
+    Key? key,
+    required this.cameras,
+  }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    initializeCamera(selectedCamera); //Initially selectedCamera = 0
+    super.initState();
+  }
+
+  late CameraController _controller; //To control the camera
+  late Future<void>
+      _initializeControllerFuture; //Future to wait until camera initializes
+  int selectedCamera = 0;
+  List<File> capturedImages = [];
+
+  initializeCamera(int cameraIndex) async {
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.cameras[cameraIndex],
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Hello world"),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // If the Future is complete, display the preview.
+                return CameraPreview(_controller);
+              } else {
+                // Otherwise, display a loading indicator.
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    if (widget.cameras.length > 1) {
+                      setState(() {
+                        selectedCamera = selectedCamera == 0 ? 1 : 0;
+                        initializeCamera(selectedCamera);
+                      });
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text('No secondary camera found'),
+                        duration: Duration(seconds: 2),
+                      ));
+                    }
+                  },
+                  icon: const Icon(Icons.switch_camera_rounded,
+                      color: Colors.white),
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    await _initializeControllerFuture;
+                    var xFile = await _controller.takePicture();
+                    setState(() {
+                      print(xFile.path);
+                      capturedImages.add(File(xFile.path));
+                    });
+                  },
+                  child: Container(
+                    height: 60,
+                    width: 60,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: Container(
+                    height: 60,
+                    width: 60,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      image: capturedImages.isNotEmpty
+                          ? DecorationImage(
+                              image: FileImage(capturedImages.last),
+                              fit: BoxFit.cover)
+                          : null,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
     );
   }
 }
