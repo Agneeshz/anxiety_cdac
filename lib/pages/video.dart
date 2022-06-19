5e23c7ca413bda6fa8b0082f0f4c772647dbfd2a
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-06-19 15:15:15 +0530

    video page with some bugs

---
+++ lib/pages/video.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class VideoPage extends StatelessWidget {
+  VideoPage({Key? key}) : super(key: key);
+  static String videoId = "dFKhWe2bBkM";
+
+  final YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: videoId,
+    flags: const YoutubePlayerFlags(
+      autoPlay: false,
+      mute: false,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: YoutubePlayer(
+          controller: _controller,
+          showVideoProgressIndicator: true,
+          liveUIColor: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
