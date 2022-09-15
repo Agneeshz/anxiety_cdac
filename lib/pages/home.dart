2afa23a3baa8c9ef459393e2236ceb34321427b3
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-09-15 18:01:08 +0530

    added back camera support

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -95,23 +95,24 @@ class _HomeState extends State<Home> {
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      // IconButton(
-                      //   onPressed: () {
-                      //     if (widget.cameras.length > 1) {
-                      //       setState(() {
-                      //         selectedCamera = selectedCamera == 0 ? 1 : 0;
-                      //         initializeCamera(selectedCamera);
-                      //       });
-                      //     } else {
-                      //       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      //         content: Text('No secondary camera found'),
-                      //         duration: Duration(seconds: 2),
-                      //       ));
-                      //     }
-                      //   },
-                      //   icon: const Icon(Icons.switch_camera_rounded,
-                      //       color: Colors.white),
-                      // ),
+                      IconButton(
+                        onPressed: () {
+                          if (widget.cameras.length > 1) {
+                            setState(() {
+                              selectedCamera = selectedCamera == 0 ? 1 : 0;
+                              initializeCamera(selectedCamera);
+                            });
+                          } else {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(const SnackBar(
+                              content: Text('No secondary camera found'),
+                              duration: Duration(seconds: 2),
+                            ));
+                          }
+                        },
+                        icon: const Icon(Icons.switch_camera_rounded,
+                            color: Colors.white),
+                      ),
                       GestureDetector(
                         onTap: () async {
                           setState(() {
