13fffe6f3f924d0349f8bcca2445a1885f18522f
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-12-08 18:36:54 +0530

    summary page UI improvement

--- lib/pages/summary.dart
+++ lib/pages/summary.dart
@@ -6,7 +6,12 @@ import 'package:anxiety_cdac/services/http_provider.dart';
 import 'package:anxiety_cdac/widgets/loading.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../constant/color.dart';
+import '../constant/constant.dart';
 
 class SummaryPage extends StatefulWidget {
   const SummaryPage({Key? key}) : super(key: key);
@@ -109,13 +114,45 @@ class _SummaryPageState extends State<SummaryPage> {
     return isLoading
         ? const LoadingScreen()
         : Scaffold(
-            appBar: AppBar(
-              title: const Text('Enter the summary of the video'),
-              automaticallyImplyLeading: false,
-            ),
+            // appBar: AppBar(
+            //   title: const Text('Enter the summary of the video'),
+            //   automaticallyImplyLeading: false,
+            // ),
             body: SafeArea(
-              child: Column(
+              child: ListView(
                 children: [
+                  Lottie.asset(
+                    'assets/lottie/pen.json',
+                    height: 200,
+                    width: 200,
+                    repeat: true,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                    child: Text(
+                      "Pen down your words",
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        textStyle: TextStyle(
+                            color: primaryTextColor,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                    child: Text(
+                      "General Instruction:\n$bullet Make a list of everything you discovered in the previous video.\n$bullet Summarize your words. Once finished, click the submit button to move on.",
+                      style: GoogleFonts.poppins(
+                        fontSize: 14,
+                        textStyle: TextStyle(
+                          color: secondaryTextColor,
+                          height: 1.5,
+                        ),
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
                   const SizedBox(
                     height: 15.0,
                   ),
@@ -124,23 +161,61 @@ class _SummaryPageState extends State<SummaryPage> {
                     child: TextFormField(
                       onChanged: (value) => {checkKey(value)},
                       maxLines: null,
-                      decoration: const InputDecoration(
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        textStyle: TextStyle(
+                          color: primaryTextColor,
+                        ),
+                      ),
+                      decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: primaryColor,
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(4.0),
+                            ),
+                          ),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.all(
                               Radius.circular(4.0),
                             ),
                           ),
-                          label: Text("Type here")),
+                          label: Text("Type here",
+                              style: GoogleFonts.poppins(
+                                  color: primaryTextColor,
+                                  fontWeight: FontWeight.w400))),
                     ),
                   ),
-                  const Spacer(),
-                  ElevatedButton.icon(
-                    icon: const Icon(Icons.navigate_next_rounded),
-                    label: const Text("Submit",
-                        style: TextStyle(color: Colors.white)),
-                    onPressed: () {
-                      submit();
-                    },
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(120, 0, 120, 0),
+                    child: ElevatedButton.icon(
+                      icon: const Icon(Icons.arrow_forward_ios),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(primaryColor),
+                        //round shape
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ),
+                        ),
+                      ),
+                      label: Text(
+                        "Submit",
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      onPressed: () {
+                        submit();
+                      },
+                    ),
                   ),
                   const SizedBox(
                     height: 50,
